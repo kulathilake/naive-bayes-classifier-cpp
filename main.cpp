69c1e891e4d9f2c1ab7c65b07500e60ea2fdfed0
kulathilake <shehanhere@gmail.com>
2023-10-26 01:03:19 +0530

    initialized with a cmake tutorial

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// A simple implementation of the Multinomial Naive Bayes Classification
+#include <cmath>
+#include <iostream>
+#include <string>
+#include "NaiveBayesClassifierConfig.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << argv[0] << " Version " << VERSION_MAJOR << "."
+                  << VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+
+    // convert input to double
+    // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
+    const double inputValue = std::stod(argv[1]);
+
+    // calculate square root
+    const double outputValue = sqrt(inputValue);
+    std::cout << "The square root of " << inputValue << " is " << outputValue
+              << std::endl;
+    return 0;
+}
